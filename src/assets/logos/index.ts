8400f9689bbab4b520b5d88af8b73d2bbbb59bd8
connor <connor@rowland.ai>
2026-06-30 18:01:20 -0700

    simplify logos

---
+++ src/assets/logos/index.ts
@@ -0,0 +1,41 @@
+import angularIcon from "./angular-icon.svg";
+import aws from "./aws.svg";
+import css from "./css.svg";
+import dockerIcon from "./docker-icon.svg";
+import go from "./go.svg";
+import html5 from "./html-5.svg";
+import java from "./java.svg";
+import javascript from "./javascript.svg";
+import microsoftAzure from "./microsoft-azure.svg";
+import mongodbIcon from "./mongodb-icon.svg";
+import nextjsIcon from "./nextjs-icon.svg";
+import nodejsIconAlt from "./nodejs-icon-alt.svg";
+import postgresql from "./postgresql.svg";
+import python from "./python.svg";
+import reactIcon from "./react.svg";
+import redis from "./redis.svg";
+import svelteIcon from "./svelte-icon.svg";
+import tailwindcssIcon from "./tailwindcss-icon.svg";
+import vue from "./vue.svg";
+
+export const TechLogos = {
+  Angular: angularIcon,
+  AWS: aws,
+  CSS3: css,
+  Docker: dockerIcon,
+  Go: go,
+  HTML5: html5,
+  Java: java,
+  JavaScript: javascript,
+  Azure: microsoftAzure,
+  MongoDB: mongodbIcon,
+  NextJS: nextjsIcon,
+  NodeJS: nodejsIconAlt,
+  PostgreSQL: postgresql,
+  Python: python,
+  React: reactIcon,
+  Redis: redis,
+  Svelte: svelteIcon,
+  Tailwind: tailwindcssIcon,
+  Vue: vue,
+};
--- src/features/skills/Skills.tsx
+++ src/features/skills/Skills.tsx
@@ -1,59 +1,41 @@
-import angularIcon from "../../assets/logos/angular-icon.svg";
-import aws from "../../assets/logos/aws.svg";
-import css from "../../assets/logos/css.svg";
-import dockerIcon from "../../assets/logos/docker-icon.svg";
-import gopher from "../../assets/logos/gopher.svg";
-import html5 from "../../assets/logos/html-5.svg";
-import java from "../../assets/logos/java.svg";
-import javascript from "../../assets/logos/javascript.svg";
-import microsoftAzure from "../../assets/logos/microsoft-azure.svg";
-import mongodbIcon from "../../assets/logos/mongodb-icon.svg";
-import nextjsIcon from "../../assets/logos/nextjs-icon.svg";
-import nodejsIconAlt from "../../assets/logos/nodejs-icon-alt.svg";
-import postgresql from "../../assets/logos/postgresql.svg";
-import python from "../../assets/logos/python.svg";
-import reactIcon from "../../assets/logos/react.svg";
-import redis from "../../assets/logos/redis.svg";
-import svelteIcon from "../../assets/logos/svelte-icon.svg";
-import tailwindcssIcon from "../../assets/logos/tailwindcss-icon.svg";
-import vue from "../../assets/logos/vue.svg";
+import { TechLogos } from "../../assets/logos";
 import SkillsIconGrid from "./skillsIcons";
 
 const coreTechnologies = [
-  { name: "Angular", src: angularIcon },
-  { name: "CSS3", src: css },
-  { name: "Docker", src: dockerIcon },
-  { name: "HTML5", src: html5 },
-  { name: "JavaScript", src: javascript },
-  { name: "Next.js", src: nextjsIcon },
-  { name: "Node.js", src: nodejsIconAlt },
-  { name: "PostgreSQL", src: postgresql },
-  { name: "Python", src: python },
-  { name: "React", src: reactIcon },
-  { name: "Redis", src: redis },
-  { name: "Tailwind CSS", src: tailwindcssIcon },
+  { name: "Angular", src: TechLogos.Angular },
+  { name: "CSS3", src: TechLogos.CSS3 },
+  { name: "Docker", src: TechLogos.Docker },
+  { name: "HTML5", src: TechLogos.HTML5 },
+  { name: "JavaScript", src: TechLogos.JavaScript },
+  { name: "Next.js", src: TechLogos.NextJS },
+  { name: "Node.js", src: TechLogos.NodeJS },
+  { name: "PostgreSQL", src: TechLogos.PostgreSQL },
+  { name: "Python", src: TechLogos.Python },
+  { name: "React", src: TechLogos.React },
+  { name: "Redis", src: TechLogos.Redis },
+  { name: "Tailwind CSS", src: TechLogos.Tailwind },
 ];
 
 const workingKnowledge = [
-  { name: "AWS", src: aws },
-  { name: "Azure", src: microsoftAzure },
-  { name: "Go", src: gopher },
-  { name: "Java", src: java },
-  { name: "MongoDB", src: mongodbIcon },
-  { name: "Vue.js", src: vue },
-  { name: "Svelte", src: svelteIcon },
+  { name: "AWS", src: TechLogos.AWS },
+  { name: "Azure", src: TechLogos.Azure },
+  { name: "Go", src: TechLogos.Go },
+  { name: "Java", src: TechLogos.Java },
+  { name: "MongoDB", src: TechLogos.MongoDB },
+  { name: "Vue.js", src: TechLogos.Vue },
+  { name: "Svelte", src: TechLogos.Svelte },
 ];
 
 export default function Skills() {
   return (
-    <section className="flex flex-col justify-center">
-      <div className="w-full max-w-6xl mx-auto p-6 my-10 bg-box-background/70 rounded-xl">
+    <section className="flex flex-col justify-center bg-olive-800">
+      <div className="w-full max-w-6xl mx-auto p-6 my-10 rounded-xl">
         <SkillsIconGrid
           name="Core Tech Stack"
           technologies={coreTechnologies}
         />
       </div>
-      <div className="w-full max-w-6xl mx-auto p-6 my-10 bg-box-background/70 rounded-xl">
+      <div className="w-full max-w-6xl mx-auto p-6 my-10 rounded-xl">
         <SkillsIconGrid name="Familiar With" technologies={workingKnowledge} />
       </div>
     </section>
